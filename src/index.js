78141089319b71ac511c91cb77b9cdccb84a17b8
T6970 <tenaldkyf315@gmail.com>
2025-08-29 20:54:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const caLib = {
-
-
-
-}
+  newGrid(length,dimension) {
+    if (dimension === 0) return 0;
+    const grid = new Array(length);
+    for (let i = 0; i < length; i++) {
+      grid.i = this.newGrid(length,dimension-1)
+    }
+    return grid
+  };
+  setCell(grid,index,value) {
+    let temp = grid;
+    for (let i = 0; i < index.length - 1; i++) {
+      temp = temp[index[i]]
+    };
+    
+  }
+};
+export default caLib;
+module.exports = caLib
