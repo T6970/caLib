cc31bb2f364a165285885a3fbd236c58e63356c4
T6970 <tenaldkyf315@gmail.com>
2025-08-30 07:57:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ const caLib = {
   newGrid(length,dimension) { // new grid
     if (dimension === 0) return 0; // 0D array is basically element
     return Array.from({ length }, () => this.newGrid(length, dimension-1)); // recursion
-  };
+  },
   setCell(grid,index,value) { // set cell
-    const temp0 = JSON.parse(JSON.stringify(grid)); // immutable quirk 
-    let temp1 = temp0
-    for (let i = 0; i < index.length - 1; i++) {
-      temp1 = temp1[index[i]]
-    };
-    temp1 = temp1[index[i]]
-    return temp0
+    if (index.length === 0) return value;
+    const [head, ...rest] = index;
+    return [
+      ...grid.slice(0, head),
+      this.setCell(grid[head], rest, value),
+      ...grid.slice(head + 1)
+    ];
   }
 };
 export default caLib;
-module.exports = caLib
