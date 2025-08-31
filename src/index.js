072ea7006228d081960124f33c4f1ad1ad31a852
T6970 <tenaldkyf315@gmail.com>
2025-08-31 17:31:38 +0800

    Make index.js more readable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
+// Note to self: comments should explain the purpose of code, not what it do.
+
+// Cellular Automata LIBrary
 const caLib = {
-  newGrid(length,dimension) { // new grid
-    if (dimension === 0) return 0; // 0D array is basically element
-    return Array.from({ length }, () => this.newGrid(length, dimension-1)); // recursion
+  
+  // new grid
+  // TODO: implement chunks
+  newGrid(length,dimension,default) {
+    
+    // edge case: terminate and return default value because the recursion would eventually decrement the dimension to 0
+    if (dimension === 0) return default;
+    
+    // recursion by calling newGrid with one less dimension
+    return Array.from({ length }, () => this.newGrid(length, dimension-1))
+    
+    // therefore this would create a square grid of a certain dimension
+    
   },
-  setCell(grid,index,value) { // set cell
-    if (index.length === 0) return value;
+  
+  
+  // set the cell of a grid
+  // TODO: support chunks
+  setCell(grid,index,value) {
+
+    // edge case: if grid is 0D directly return the value
+    if (index.length === 0) return [value];
+
+    // 
     const [head, ...rest] = index;
+    
+    // 
     return [
       ...grid.slice(0, head),
       this.setCell(grid[head], rest, value),
       ...grid.slice(head + 1)
     ];
-  }
+    
+  },
+
+  // TODO: support B/S and Hensel notation
+  toRule(rulestring) {},
+
+  // TODO: implement step algoritm with O(1) complexity
+  step(grid,rule) {},
 };
+
+
 export default caLib;
